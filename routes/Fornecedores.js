50e4c1952566c5d479b5218f8087fcfe21b0e17b
Gustavo de Oliveira Maciel <133970388+Perniciem@users.noreply.github.com>
2024-03-07 16:32:13 -0300

    Update Fornecedores.js

--- routes/Fornecedores.js
+++ routes/Fornecedores.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/Fornecedores', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('Envio para todo tipo de loja.');
 });
 
-router.get('/Fornecedores/Produtos', function(req, res, next) {
+router.get('/Produtos', function(req, res, next) {
   res.send('Aqui estão meus produtos');
 });
 
-router.get('/Fornecedores/Promocoes', function(req, res, next) {
+router.get('/Promocoes', function(req, res, next) {
   res.send('Aqui as promoções do dia.');
 });
 
-router.get('/Fornecedores/Listagem', function(req, res, next) {
+router.get('/Listagem', function(req, res, next) {
   res.send('Aqui as listas de vendas.');
 });
 
