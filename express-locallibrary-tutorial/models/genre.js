252d81b9582c61e68dd2d510fbcd533b27a731a8
Redes IFC <redes@lab32-pc01.sombrio.ifc.edu.br>
2024-03-22 15:11:22 -0300

    Gustavo 3A

--- express-locallibrary-tutorial/models/genre.js
+++ express-locallibrary-tutorial/models/genre.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const GenreSchema = new Schema({
-  name: { type: String, required: true, mininumLenght: 3, maxLength: 100 },
+  name: { type: String, required: true, minLenght: 3, maxLength: 100 },
 });
 
 GenreSchema.virtual("name").get (function (){
     // We don't use an arrow function as we'll need the this object
     return `/catalog/genre/${this._id}`;
-  })
+  });
 
 // Export model
 module.exports = mongoose.model("Genre", GenreSchema);
